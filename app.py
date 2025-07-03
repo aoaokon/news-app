71dfcd3d0a00a33d1618e11963c69cfa1239d02b
aoaokon <aoiwatanabe78@gmail.com>
2025-07-03 10:40:33 +0900

    Add files for Render deployment

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def index():
 
     return render_template('index.html', articles=articles, keyword=keyword, remaining=remaining, lang=lang)
 
-if __name__ == '__main__':
-    app.run(debug=True) # デバッグモードでアプリを実行
+#if __name__ == '__main__':
+#    app.run(debug=True) # デバッグモードでアプリを実行
